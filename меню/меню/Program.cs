a9cdc65027351b45cb4f442ed337a3fedb67f7a0
shilova26 <58134726+shilova26@users.noreply.github.com>
2020-02-19 10:51:46 +0700

    Add files via upload

---
+++ меню/меню/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace меню
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            int i;
+
+            {
+
+                {
+                    Console.Write("Меню:\n1) Имя \n2) Фамилия \n3) Возраст \nВведите номер: ");
+                    i = int.Parse(Console.ReadLine());
+                    switch (i)
+                    {
+                        case 1:
+                            Console.WriteLine("Введите имя");
+                            Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Введите фамилию ");
+                            Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Введите возраст");
+                            Console.ReadLine();
+                            break;
+                    }
+                            
+                    Console.Write("\nНажмите любую клавишу, чтобы выйти: ");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+            }
+        }
+    }
+}
