62b5639ef9899737e00c55c2716009dda91b4994
shilova26 <58134726+shilova26@users.noreply.github.com>
2020-02-19 10:42:34 +0700

    Add files via upload

---
+++ практическая 6/практическая 6/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+
+namespace практическая_6
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n=10;
+            int m=15;
+            int[,] a = CreateArray(n, m, 10);
+            ShowArray(a, n, m);
+            int[] b = new int[m];
+            int []c = new int[n];
+            int []meanValue = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int s = 0;
+                int p = 1;
+                for (int j = 0; j <m ; j++)
+                {
+                    p *= a[i, j];
+                    s += a[i, j];
+                    c[i] = p;
+                    meanValue[i] = s / m;
+                }
+             Console.WriteLine("znacheniya: " + b[i]+ ", "+c[i]+ ", "+meanValue[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int s = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    b[j]+= a[i, j];
+
+                }
+                b[i] = s;
+
+            }
+        }
+        static int[,] CreateArray(int n, int m, int value)
+        {
+            int[,] a = new int[n, m];
+            var rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    a[i, j] = rand.Next(value);
+                }
+            }
+            return a;
+        }
+        static void ShowArray(int[,] a, int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ практическая 7/практическая 7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace практическая_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5, 7];
+
+
+        }
+    }
+}
